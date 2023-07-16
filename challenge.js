f44679b3cb421ed4d041f0cf0e93a58ab8e3c6ee
rehhana <ashiqtalmustahiil@gmail.com>
2023-07-15 22:35:39 -0700

    answere of two number

--- challenge.js
+++ challenge.js
@@ -3,7 +3,9 @@
 let number=30
 Number=50
 console.log(number,Number)
-if (number < Number){
+if (number > Number){
+    console.log(`${Number} is larger then ${number}`)
+}else{
     console.log('The largest number out of the two numbers is ',Number)
 }
 
